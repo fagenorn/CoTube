39ed1a9f199945a51c1f4cd5556463f0484fcaa0
fagenorn <anisanissakkaf@gmail.com>
2017-03-26 12:07:11 +0200

    Initial Commit

---
+++ CoTube/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MainWindow.xaml.cs" company="GMT">
+//   Created by Fagenorn
+// </copyright>
+// <summary>
+//   Interaction logic for MainWindow.xaml
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace CoTube
+{
+    /// <summary>
+    ///     The main window.
+    /// </summary>
+    public partial class MainWindow
+    {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="MainWindow" /> class.
+        /// </summary>
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
